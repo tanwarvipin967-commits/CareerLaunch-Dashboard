1a32273d159e93f689474ee7a2a49e9f824c0d7f
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-05-03 11:36:39 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Ye line add karo
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Ye line add karo
-  ],
+  plugins: [react()],
 })
