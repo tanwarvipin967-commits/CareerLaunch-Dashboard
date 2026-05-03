05c1c7a172e796a8087cce63c5f478ee3f0b537b
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-05-03 10:32:41 +0530

    Update Index.css code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Ye line add karo
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Ye line add karo
+  ],
 })
